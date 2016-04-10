77f87765cdf7acfede65b727fae57be4a4e3e3c3
Hassan Albalawi <Hassan@ksa.us.com>
2016-04-11 00:28:27 +0300

    Upload files

---
+++ OpenBCI-AlphaWaves (iOS)/OpenBCI-AlphaWaves/OpenBCI-AlphaWaves/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenBCI-AlphaWaves
+//
+//  Created by Hassan Albalawi on 4/9/16.
+//  Copyright © 2016 Hassan Albalawi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
